3ff1598882b78ea424ce15427902446d1bf853f4
Diego Rojas <diego@localhost.localdomain>
2017-11-19 18:28:03 -0500

    First mockup of the app

---
+++ src/pages/estacion/estacion.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-estacion',
+  templateUrl: 'estacion.html'
+})
+export class Estacion {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Estacion } from '../estacion/estacion';
 
 @Component({
   selector: 'page-home',
@@ -10,5 +11,11 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
+
+  seleccionarEstacion ( $event ) : void {
+    this.navCtrl.push( Estacion );
+  }
+
+
 
 }
